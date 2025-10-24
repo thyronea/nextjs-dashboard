5889bf4fae96415f8713cfff09b54ad46df2609a
thyronea <thyrone.antonio@gmail.com>
2025-10-24 12:17:06 -0700

    Chapter 16: Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from 'next';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense} from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton, } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
  
 export default async function Page() {
   return (
